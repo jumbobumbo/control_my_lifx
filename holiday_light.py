e9359c7e93e6fd9c83c46a213240c303aa6fbf23
jumbobumbo <MasterCh0der!*>
2020-01-08 22:06:02 +0000

    fixed incorrect dictionary path

--- holiday_light.py
+++ holiday_light.py
@@ -10,7 +10,8 @@ c_time = datetime.now().strftime("%H:%M:%S")
 with RCL([k for k, _ in config["lights"].items()]) as lights:
     for light in lights.devices:
         # are we within active hours?
-        if config["active_hours"]["group_1"]["start"] <= c_time < config["active_hours"]["group_1"]["end"]:
+        if config["active_hours"][config["lights"][light.label]["group"]]["start"] <= c_time < \
+                config["active_hours"][config["lights"][light.label]["group"]]["end"]:
             # if color[2] (brightness) is lower than config - turn on - if its the same, leave it alone
             if light.get_color()[2] < config["lights"][light.label]["color"][2]:
                 light.set_color(config["lights"][light.label]["color"])
